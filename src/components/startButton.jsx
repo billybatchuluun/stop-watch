b7efa7c4f2cdd63c35b32bace4726d804d7b1e9c
Bilguun Batchuluun <23LP6327@G304-CC0073.local>
2023-12-18 18:38:55 +0800

    first commit

---
+++ src/components/startButton.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const startButton = () => {
+  return (
+    <div className="flex w-10 h-10 bg-slate-400 justify-center text-white">
+      <p className="flex items-center ">Start</p>
+    </div>
+  );
+};
